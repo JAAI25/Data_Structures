fd109a4b5929c7c055ab7b282364e97e58b0b556
JAAI25 <jaigawade96@gmail.com>
2025-12-29 20:05:07 +0530

    Add files via upload

---
+++ Java_Programming/DoublyCircular_Linklist.java
@@ -0,0 +1,290 @@
+  //Doubly Circular
+
+class node
+{
+    public int data;
+    public node next;
+    public node prev;
+
+    public node(int no)
+    {
+        this.data = no;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyCL
+{
+    private node first;
+    private node last;
+    
+    private int iCount;
+
+    public DoublyCL()
+    {
+        System.out.println("Object of DoublyCL gets created.");
+        this.first = null;
+        this.last = null;
+        this.iCount = 0;
+    }
+
+    public void InsertFirst(int no)
+    { 
+        node newn = null;
+
+        newn = new node(no);
+
+        if(this.first == null && this.last == null) 
+        {
+            this.first = newn;
+            this.last = newn;
+        }
+        else
+        {
+            newn.next = this.first;
+            newn.prev = this.last;
+
+            this.first = newn;
+
+        }
+        this.iCount++;
+        this.last.next = this.first;
+        this.first.prev = this.last;
+    }
+
+    public void InsertLast(int no)
+    {
+        node newn = null;
+
+        newn = new node(no);
+
+        if(this.first == null && this.last == null) 
+        {
+            this.first = newn;
+            this.last = newn;
+        }
+        else
+        {
+            newn.prev = this.last;
+            newn.next = this.first;
+
+            this.last.next = newn;
+            this.first.prev = newn;
+
+            this.last = newn;
+        }
+        this.iCount++;
+        this.last.next = this.first;
+        this.first.prev = this.last;
+        
+    }
+
+    public void DeleteFirst()
+    {
+        if(this.first == null && this.last == null )
+        {
+            return;
+        }
+        else if(this.first == this.last)
+        {
+            this.first = null;
+            this.last = null;
+        }
+        else
+        {
+            this.first = this.first.next;
+
+            this.last.next = this.first;
+            this.first.prev = this.last;
+
+        }
+        this.iCount--;
+    }
+
+    public void DeleteLast()
+    {
+        if(this.first == null && this.last == null )
+        {
+            return;
+        }
+        else if(this.first == this.last)
+        {
+            this.first = null;
+            this.last = null;
+        }
+        else
+        {
+            this.last = this.last.prev;
+
+            this.last.next = this.first;
+            this.first.prev = this.last;
+
+        }
+        this.iCount--;
+    }
+
+    public void Display()
+    {
+        node temp = null;
+
+        if(first == null)
+        {
+            System.out.println("Linklist is empty.");
+            return;
+        }
+
+        temp = this.first;
+        System.out.print("<=>");
+        do 
+        {
+            System.out.print(" |" +temp.data+ " |<=>");
+            temp = temp.next;
+
+        }while(temp != this.first);
+        System.out.println();
+
+    }
+
+    public int Count()
+    {
+        return this.iCount;
+    }
+
+    public void InsertAtPos(int no, int pos)
+    {
+        node temp = null;
+        node newn = null;
+
+        int iCnt = 0;
+        if(pos < 1 || pos > iCount + 1)
+        {
+            System.out.println("Invalid position.");
+            return;
+        }
+
+        if(pos == 1)
+        {
+            this.InsertFirst(no);
+        }
+        else if(pos == iCount + 1)
+        {
+            this.InsertLast(no);
+        }
+        else
+        {
+            newn = new node(no);
+
+            temp = this.first;
+            for(iCnt = 1; iCnt < pos-1; iCnt++)
+            {
+                temp = temp.next;
+            }
+            newn.next = temp.next;
+            newn.prev = temp;
+
+            temp.next.prev = newn;
+            temp.next = newn;
+
+            this.iCount++;
+
+        }
+    }
+
+    public void DeleteAtPos(int pos)
+    {
+        node temp = null;
+        node target = null;
+
+        int iCnt = 0;
+
+        if(pos < 1 || pos > iCount)
+        {
+            System.out.println("Invalid position.");
+            return;
+        }
+
+        if(pos == 1)
+        {
+            this.DeleteFirst();
+        }
+        else if(pos == iCount)
+        {
+            this.DeleteLast();
+        }
+        else
+        {
+            temp = this.first;
+            for(iCnt = 1; iCnt < pos-1; iCnt++)
+            {
+                temp = temp.next;
+            }
+            target = temp.next;
+            temp.next = target.next;
+            temp.next.prev = temp;
+            this.iCount--;
+
+        }
+    }
+}
+
+class program449
+{
+    public static void main(String A[])
+    {
+        int iRet = 0;
+        DoublyCL obj = null;
+
+        obj = new DoublyCL();
+
+        obj.InsertFirst(51);
+        obj.InsertFirst(21);
+        obj.InsertFirst(11);
+
+        obj.Display();
+
+        iRet = obj.Count();
+        System.out.println("Number of nodes are :"+iRet);
+
+        obj.InsertLast(101);
+        obj.InsertLast(111);
+        obj.InsertLast(121);
+
+        obj.Display();
+
+        iRet = obj.Count();
+        System.out.println("Number of nodes are :"+iRet);
+
+        obj.DeleteFirst();
+
+        obj.Display();
+
+        iRet = obj.Count();
+        System.out.println("Number of nodes are :"+iRet);
+
+        obj.DeleteLast();
+
+        obj.Display();
+
+        iRet = obj.Count();
+        System.out.println("Number of nodes are :"+iRet);
+
+        obj.InsertAtPos(105,4);
+
+        obj.Display();
+
+        iRet = obj.Count();
+        System.out.println("Number of nodes are :"+iRet);
+
+        obj.DeleteAtPos(4);
+
+        obj.Display();
+
+        iRet = obj.Count();
+        System.out.println("Number of nodes are :"+iRet);
+
+        //Important for memory deallocation
+        obj = null;
+        System.gc();
+    }
+}
